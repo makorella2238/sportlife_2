02e8c1110763e0b22b7cb2a1ee61016fdf502f58
makorella <makorella2238@gmail.com>
2025-07-07 11:29:57 +0300

    fixed

--- src/skins/style_1/mid.tsx
+++ src/skins/style_1/mid.tsx
@@ -179,7 +179,7 @@ const Header = styled.h1`
 const BottomWrapper = styled.div`
   position: absolute;
   bottom: 50px; /* Прижать снизу с отступом */
-  left: 20px; /* По левому краю, как у LogoBottom */
+  left: 14px; /* По левому краю, как у LogoBottom */
   display: flex;
   flex-direction: column;
   align-items: flex-start; /* Чтобы текст и лого были слева */
